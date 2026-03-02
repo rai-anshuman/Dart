3047143cd0f4b79cd45c7473063c530d5a8f231f
rai-anshuman <anshumanr699@gmail.com>
2026-03-03 01:44:07 +0530

    Add files via upload

---
+++ tempCodeRunnerFile.dart
@@ -0,0 +1,21 @@
+
+// class Swimmer {
+//   void swim() => print("Swimming");
+// }
+
+// class Runner {
+//   void run() => print("Running");
+// }
+
+// class Athlete implements Swimmer, Runner {
+//   @override
+//   void run() => print("Running like an athlete");
+
+//   @override
+//   void swim() => print("Swimming like an athlete");
+// }
+// void main() {
+//   var athlete = Athlete();
+//   athlete.swim();
+//   athlete.run();
+// }
